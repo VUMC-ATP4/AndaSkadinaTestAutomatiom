2b8002c196d0bfb65b0fc239510980ef5980270c
anda-skadina <104780367+anda-skadina@users.noreply.github.com>
2022-06-27 20:03:02 +0300

    Update SeleniumTest.java

--- src/test/java/classroomSeven/SeleniumTest.java
+++ src/test/java/classroomSeven/SeleniumTest.java
@@ -3,7 +3,7 @@ package classroomSeven;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+//koments
 public class SeleniumTest {
 
     @Test
